82c1e97b0d0a7ffe4937ad1d6b886e0c1c264d9a
WesselB1234 <stosin007@hotmail.com>
2025-08-21 17:28:39 +0200

    Total value serverside

--- RealTimeStockSimulator/Models/Ownership.cs
+++ RealTimeStockSimulator/Models/Ownership.cs
@@ -4,6 +4,20 @@
     {
         public User User;
         public List<OwnershipTradable> Tradables;
+        public decimal TotalOwnershipValue
+        {
+            get
+            {
+                decimal total = 0;
+
+                foreach (OwnershipTradable tradable in Tradables)
+                {
+                    total += tradable.TotalValue;
+                }
+
+                return total;
+            }
+        }
 
         public Ownership(User user, List<OwnershipTradable> tradables)
         {
--- RealTimeStockSimulator/Models/OwnershipTradable.cs
+++ RealTimeStockSimulator/Models/OwnershipTradable.cs
@@ -3,6 +3,18 @@
     public class OwnershipTradable : Tradable
     {
         public int Amount;
+        public decimal TotalValue
+        {
+            get
+            {
+                if (TradablePriceInfos != null)
+                {
+                    return TradablePriceInfos.Price * Amount;
+                }
+
+                return 0;
+            }
+        }
 
         public OwnershipTradable(string symbol, int amount) : base(symbol)
         {
