639a416a3dd052e6f5d216289d0c888db15789f3
WesselB1234 <stosin007@hotmail.com>
2026-01-03 00:01:25 +0100

    AuthenticatedUserControlled added to remove duplicated code

---
+++ RealTimeStockSimulator/Controllers/AuthenticatedUserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using RealTimeStockSimulator.Models;
+using RealTimeStockSimulator.Services.Interfaces;
+
+namespace RealTimeStockSimulator.Controllers
+{
+    public class AuthenticatedUserController : Controller
+    {
+        protected UserAccount LoggedInUser;
+        private IUsersService _usersService;
+
+        public AuthenticatedUserController(IUsersService usersService)
+        {
+            _usersService = usersService;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            LoggedInUser = _usersService.GetUserFromClaimsPrinciple(User);
+
+            ViewBag.loggedInUser = LoggedInUser;
+            base.OnActionExecuting(context);
+        }
+    }
+}
--- RealTimeStockSimulator/Controllers/PortfolioController.cs
+++ RealTimeStockSimulator/Controllers/PortfolioController.cs
@@ -1,35 +1,22 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
-using RealTimeStockSimulator.Models;
 using RealTimeStockSimulator.Services.Interfaces;
 
 namespace RealTimeStockSimulator.Controllers
 {
     [Authorize]
-    public class PortfolioController : Controller
+    public class PortfolioController : AuthenticatedUserController
     {
         private IOwnershipsService _ownershipsService;
-        private IUsersService _usersService;
-        private UserAccount _loggedInUser;
 
-        public PortfolioController(IOwnershipsService ownershipsService, IUsersService usersService)
+        public PortfolioController(IOwnershipsService ownershipsService, IUsersService usersService): base(usersService)
         {
             _ownershipsService = ownershipsService;
-            _usersService = usersService;
-        }
-
-        public override void OnActionExecuting(ActionExecutingContext context)
-        {
-            _loggedInUser = _usersService.GetUserFromClaimsPrinciple(User);
-
-            ViewBag.loggedInUser = _loggedInUser;
-            base.OnActionExecuting(context);
         }
 
         public IActionResult Index()
         {
-            return View(_ownershipsService.GetAllOwnershipTradablesByUserId(_loggedInUser.UserId));
+            return View(_ownershipsService.GetAllOwnershipTradablesByUserId(LoggedInUser.UserId));
         }
     }
 }
